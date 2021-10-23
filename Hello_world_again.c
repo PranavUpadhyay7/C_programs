2b37ea9b1effd142e857d7aa1c3b541b62fa9ad8
PranavUpadhyay7 <91959702+PranavUpadhyay7@users.noreply.github.com>
2021-10-23 16:09:38 +0530

    Create Hello_world_again.c

---
+++ Hello_world_again.c
@@ -0,0 +1,12 @@
+
+//  Coding.c
+//
+//  Created by Pranav Upadhyay on 23/10/21.
+#include <stdio.h>
+
+int cdf(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
